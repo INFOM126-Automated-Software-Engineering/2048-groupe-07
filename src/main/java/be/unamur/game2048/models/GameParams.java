c53b0c66885cc2d4763a57e067755b37e8d762b9
AlineBlg <aline.boulanger@student.unamur.be>
2024-12-12 17:03:14 +0100

    Test de la github actions Maven
    Ajout d'un commentaire et d'un print pour tester la github actions Maven

--- src/main/java/be/unamur/game2048/models/GameParams.java
+++ src/main/java/be/unamur/game2048/models/GameParams.java
@@ -4,4 +4,8 @@ public class GameParams {
     public static final int sideLength = 4;
     public static final int nbStartTileFilled = 2;
     public static final int scoreTarget = 2048;
+
+    public static void main(String[] args) {
+        System.out.println("nbStartTileFilled: " + nbStartTileFilled);
+    }
 }
