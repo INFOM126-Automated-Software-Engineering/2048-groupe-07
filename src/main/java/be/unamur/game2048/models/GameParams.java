f04ffdebb1136da451b89c1c064e207bf7f19cf1
AlineBlg <aline.boulanger@student.unamur.be>
2024-12-18 17:24:50 +0100

    adieu fonction pour print avec System.out.println
    Co-Authored-By: ldelpier <123209833+ldelpier@users.noreply.github.com>

--- src/main/java/be/unamur/game2048/models/GameParams.java
+++ src/main/java/be/unamur/game2048/models/GameParams.java
@@ -5,7 +5,4 @@ public class GameParams {
     public static final int nbStartTileFilled = 2;
     public static final int scoreTarget = 2048;
 
-    public static void main(String[] args) {
-        System.out.println("nbStartTileFilled: " + nbStartTileFilled);
-    }
 }
